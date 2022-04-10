67e5b31a62d549608e08d8401db06d8d68f378b9
qinbiao11125 <1052080761@qq.com>
2022-04-10 16:08:15 +0800

    增加登录初始化逻辑

--- ChatServer/ChatServer.cpp
+++ ChatServer/ChatServer.cpp
@@ -1,10 +1,11 @@
 #include "ChatServer.h"
+#include <iostream>
 
 ChatServer::ChatServer(io_context& io_ctx,short port)
-:m_acceptor(io_ctx,tcp::endpoint(tcp::v4(),port))
+:m_ioc(io_ctx),
+m_acceptor(io_ctx,tcp::endpoint(tcp::v4(),port))
 {
     DoAccept();
-    //HFDAKJH
 }
 
 void ChatServer::DoAccept()
@@ -13,10 +14,40 @@ void ChatServer::DoAccept()
         [this](std::error_code ec,tcp::socket socket){
             if(!ec)
             {
-                auto new_Acceptor=std::make_shared<ChatClient>(std::move(socket));
+                auto new_Acceptor=std::make_shared<ChatClient>(std::move(socket),this,m_ioc);
+                m_socketVec.push_back(new_Acceptor);
                 new_Acceptor->Start();
             }
             DoAccept();
         }
     );
+}
+
+void ChatServer::removeDisconnetedClient(int id,std::shared_ptr<ChatClient> ptr)
+{
+    if(m_idMap.find(id)!=m_idMap.end())
+    {
+        m_idMap.erase(id);
+    }
+    printf("before size is:%d\n",m_socketVec.size());
+    for(auto iter=m_socketVec.begin();iter!=m_socketVec.end();++iter)
+    {
+        if(*iter==ptr)
+        {
+            std::cout<<(*iter).use_count()<<std::endl;
+            m_socketVec.erase(iter);
+            return;
+        }
+    }
+    printf("after size is:%d\n",m_socketVec.size());
+}
+
+void ChatServer::insertIntoIdMap(int id,std::shared_ptr<ChatClient> m_clientPtr)
+{
+    m_idMap[id]=m_clientPtr;
+}
+
+bool ChatServer::transferMessage(int id,std::string& message)
+{
+    m_idMap[id]->DoWrite(message,message.length());
 }
--- ChatServer/ChatServer.h
+++ ChatServer/ChatServer.h
@@ -1,13 +1,20 @@
 #include "ChatClient.h"
 #include <unordered_map>
+#include <vector>
 
 class ChatServer
 {
 public:
     ChatServer(io_context& io_ctx,short port);
     void DoAccept();
-
+    void removeDisconnetedClient(int id,std::shared_ptr<ChatClient> ptr);
+    bool ChangeUserStateInDB(int id);
+    bool transferMessage(int id,std::string& message);
+    //发送初始化消息后把client保存在id对应的map中
+    void insertIntoIdMap(int id,std::shared_ptr<ChatClient> m_clientPtr);
 private:
     tcp::acceptor m_acceptor;
-    static std::unordered_map<int,std::shared_ptr<ChatClient>> m_userMap;
+    std::vector<std::shared_ptr<ChatClient>> m_socketVec;   //存储的是连接上来的socket，不一定有id，可能是注册的时候连接上来的
+    std::unordered_map<int,std::shared_ptr<ChatClient>> m_idMap;//每个socket对应的id，也就是对应的用户id
+    boost::asio::io_context& m_ioc;
 };
