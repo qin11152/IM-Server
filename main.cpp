afba6a8ccae46f5ffc507aea97d5afa90e3ebe39
qin11152 <1052080761@qq.com>
2023-04-02 16:09:22 +0800

    增加命名空间的处理

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Author: qin11152 1052080761@qq.com
+ * @Date: 2022-06-18 13:57:30
+ * @LastEditors: qin11152 1052080761@qq.com
+ * @LastEditTime: 2023-03-21 20:57:26
+ * @FilePath: /IM-Server/main.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #include "ChatServer/ChatServer.h"
 #include "MysqlQuery.h"
 #include "Log.h"
@@ -9,16 +17,11 @@
 
 int main(int argc,char* argv[])
 {
-    if(argc!=2)
-    {
-        std::cout<<"usage: "<<argv[0]<<" port\n";
-        return 1;
-    }
     Logcxx::get_mutable_instance().Init("./log4cxx.properties");
-    unsigned short port=std::atoi(argv[1]);
+    unsigned short port=9999;
     boost::asio::io_context io;
-    ChatServer server(io,port);
-    for(int i=0;i<5;++i)
+    net::ChatServer server(io,port);
+    for(int i=0;i<10;++i)
     {
         std::thread t([&](){io.run();});
         t.detach();
