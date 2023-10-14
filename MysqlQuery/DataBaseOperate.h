9bc96521267e9ebc6f1985b56284f310d6f6fe00
qin11152 <1052080761@qq.com>
2023-10-14 16:12:50 +0800

    [MOD] 修改头文件引用

--- MysqlQuery/DataBaseOperate.h
+++ MysqlQuery/DataBaseOperate.h
@@ -1,13 +1,18 @@
+/*
+ * @Author: qin11152 1052080761@qq.com
+ * @Date: 2023-10-12 20:08:26
+ * @LastEditors: qin11152 1052080761@qq.com
+ * @LastEditTime: 2023-10-14 15:46:15
+ * @FilePath: /IM-Server/MysqlQuery/DataBaseOperate.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #pragma once
 
 #include "Mydefine.h"
-#if defined(__linux__)
-    #include <mysql/mysql.h>
-    #include <mysql/mysqld_error.h>
-#elif defined(_WIN32)
-    #include <mysql.h>
-    #include <mysqld_error.h>
-#endif
+#include "module/Log.h"
+
+#include <mysql/mysql.h>
+#include <mysql/mysqld_error.h>
 #include <stdio.h>
 #include <string>
 #include <iostream>
